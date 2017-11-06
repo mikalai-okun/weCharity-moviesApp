ea1e876a58f4ad9bc9bb8697b51361251b5570ce
mikalai_okun <mickokun@gmail.com>
2017-11-05 22:47:02 -0500

    Added Functional Tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -2,10 +2,31 @@
 
 namespace Tests\AppBundle\Controller;
 
+use Abibockun\SimpleCurlConnector\SimpleCurlConnector;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    public function testApiConnection()
+    {
+        $client = static::createClient();
+
+        $curl = new SimpleCurlConnector();
+        $curl->setEndPointBaseUrl($client->getKernel()->getContainer()->getParameter('themoviedb_endpoint_url'));
+        $curl->setExtraHeaders([
+            CURLOPT_HEADER => false,
+            CURLOPT_HTTPHEADER => ["Accept: application/json"],
+            CURLOPT_SSL_VERIFYPEER => false
+        ]);
+
+        $movies = $curl->send(
+            '/movie/popular?api_key='.$client->getKernel()->getContainer()->getParameter('themoviedb_api_key')
+        );
+
+        $this->assertInternalType('array',$movies->results);
+        $this->assertNotEmpty($movies->results);
+    }
+
     public function testIndex()
     {
         $client = static::createClient();
@@ -20,7 +41,7 @@ class DefaultControllerTest extends WebTestCase
             ->eq(1)
             ->link()
         ;
-        dump($link);
         $crawler = $client->click($link);
+        $this->assertContains('Mark as favorite', $crawler->text());
     }
 }
